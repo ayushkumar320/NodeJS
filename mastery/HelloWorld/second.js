91258d0b2e9c805179a5c0da68a4590a5f9606d9
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-04-19 14:37:42 +0530

    import and export modules

---
+++ mastery/HelloWorld/second.js
@@ -0,0 +1,8 @@
+// const math = require('./math');
+const {add, sub} = require('./math');
+
+// console.log(`The value of math is ${math}`);
+// console.log(math.addFn(3, 5));
+// console.log(math.subFn(3, 5));
+console.log(add(3, 5));
+console.log(sub(3, 5));
